ae4d53a0325cfceaef485ce998a5baf632f2f25f
rahulthakur01 <abhisharma7091@gmail.com>
2025-09-14 03:23:57 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,20 @@ function App() {
 
   return (
     <>
+<<<<<<< Updated upstream
      <div className="min-h-screen  bg-richblue-800  flex items-center justify-center">
       <h1 className="text-8xl text-richblack-200">Hello Rohit</h1>
     </div>
 
+=======
+    <h1 className='text-4xl font-bold text-richblue-300'>
+      Hello Rohit
+    </h1>
+>>>>>>> Stashed changes
     </>
   )
 }
 
-export default App
-
-
-
-
-
-
-
-
+export default App;
 
 

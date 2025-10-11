220b00bed5ec81fec1855f034502632e5be91de0
Rahul Kumar <rahul.th7632@gmail.com>
2025-10-11 17:11:45 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-
+import Navbar from "./Component/Common/Navbar";
 function App() {
   return (
     <>
       <div className=" w-screen min-h-screen bg-richblack-900 font-inter">
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           
--- src/Component/Common/Navbar.jsx
+++ src/Component/Common/Navbar.jsx
@@ -1,5 +1,67 @@
-import React from 'react'
-const Navbar =()=>{
+import React from "react";
+import { NavbarLinks } from "../../data/navbar-links";
+import { Link, matchPath, useLocation } from "react-router-dom";
+import { FaChevronDown } from "react-icons/fa";
 
-}
-export default Navbar
+const Navbar = () => {
+  const location = useLocation();
+
+  
+  function matchRoute(route) {
+    return matchPath({ path: route }, location.pathname);
+  }
+  return (
+    <>
+      <div className="h-16 flex jusitfy-center items-center border border-b-[1px] border-b-richblack-700 ">
+        <div className="flex justify-between items-center w-11/12 max-w-[1260px] mx-auto">
+          <Link to="/" className="text-white font-bold text-xl">
+            LearnSphere
+          </Link>
+          <nav>
+            <ul className="flex gap-x-6 text-richblack-25">
+              {NavbarLinks.map((link, index) => {
+                return (
+                  <li className="text-white">
+                    {link.title === "Catalog" ? (
+                      <div className="flex items-center gap-2 relative group">
+                        <p>{link.title}</p>
+                        <FaChevronDown />
+                        <div
+                          className="w-[300px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[70%]
+                        rounded-md bg-richblack-5 p-4 text-richblack-900 invisible group-hover:visible "
+                        >
+                          {" "}
+                          <div
+                            className="absolute left-[50%] top-0
+                              translate-x-[80%]
+                              translate-y-[-45%] h-6 w-6 rotate-45 rounded bg-richblack-5"
+                          ></div>
+
+
+                        </div>
+                      </div>
+                    ) : (
+                      <Link to={link?.path}>
+                        <p
+                          className={`${
+                            matchRoute(link?.path)
+                              ? "text-yellow-25"
+                              : "text-richblack-25"
+                          }`}
+                        >
+                          {link.title}
+                        </p>
+                      </Link>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+          <div>lgoi</div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Navbar;

c6535a9831110cb6c829d26bac9ecfbc2397239a
Rahul Kumar <rahul.th7632@gmail.com>
2025-10-23 18:26:03 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,33 +3,52 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Navbar from "./Component/Common/Navbar";
-import Login from "./Pages/Login"
+import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import OpenRoute from "./Component/Core/HomePage/Auth/OpenRoute";
 import VerifyEmail from "./Pages/VerifyEmail";
+import ForgetPassword from "./Pages/ForgetPassword";
 function App() {
   return (
     <>
       <div className=" w-screen min-h-screen bg-richblack-900 font-inter">
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="signup" element={
-            <OpenRoute>
-              <Signup/>
-            </OpenRoute>
-          }/>
-            <Route path="login" element={
-            <OpenRoute>
-              <Login/>
-            </OpenRoute>
-          }/>
+          <Route
+            path="signup"
+            element={
+              <OpenRoute>
+                <Signup />
+              </OpenRoute>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <OpenRoute>
+                <Login />
+              </OpenRoute>
+            }
+          />
 
-          <Route path="/verify-email" element={
-            <OpenRoute>
-              <VerifyEmail/>
-            </OpenRoute>
-          }/>
+          <Route
+            path="/verify-email"
+            element={
+              <OpenRoute>
+                <VerifyEmail />
+              </OpenRoute>
+            }
+          />
+
+          <Route
+            path="/forgetPassword"
+            element={
+              <OpenRoute>
+                <ForgetPassword />
+              </OpenRoute>
+            }
+          />
         </Routes>
       </div>
     </>
--- client/src/Pages/VerifyEmail.jsx
+++ client/src/Pages/VerifyEmail.jsx
@@ -19,7 +19,9 @@ const VerifyEmail = () => {
             <p className="text-[1.125rem] leading-[1.625rem] my-4 text-richblack-100">
               A verification code has been sent to you. Enter the code below
             </p>
-            <form></form>
+            <form>
+             
+            </form>
             <button
               type="submit"
               className="w-full bg-yellow-50 py-[12px] px-[12px] rounded-[8px] mt-6 font-medium text-richblack-900"
@@ -28,10 +30,16 @@ const VerifyEmail = () => {
             </button>
 
             <div className="flex gap-4 items-center justify-between mt-6">
-              <Link to="/signup" className="flex items-center gap-x-2 text-richblack-5">
-                <FaArrowLeft /><p className=" ">Back to Signup</p>
+              <Link
+                to="/signup"
+                className="flex items-center gap-x-2 text-richblack-5"
+              >
+                <FaArrowLeft />
+                <p className=" ">Back to Signup</p>
               </Link>
-              <button className="text-blue-200 cursor-pointer">Resend it</button>
+              <button className="text-blue-200 cursor-pointer">
+                Resend it
+              </button>
             </div>
           </div>
         )}

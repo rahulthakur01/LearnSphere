a33e246c7e870b062ff172e729d50d77f8f6cc9a
Rahul Kumar <rahul.th7632@gmail.com>
2025-11-26 15:28:11 +0530

    updated

--- client/src/Component/Core/Dashboard/Cart/RenderCartCourses.jsx
+++ client/src/Component/Core/Dashboard/Cart/RenderCartCourses.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import ReactStars from "react-rating-stars-component";
+import {GiNinjaStar} from "react-icons/gi"
+import {RiDeleteBin6Line} from "react-icons/ri"
 
 const RenderCartCourses = () => {
+  const { cart } = useSelector((state) => state.cart);
   return (
-   <>
-    <div>RenderCartCourses</div>
-   </>
-  )
-}
+    <>
+      <div>
+        {cart.map((course, i) => (
+          <div>
 
-export default RenderCartCourses
+            <div>
+              <div>
+                <img src={course?.thumbnail} />
+              </div>
+
+              <div>
+                <h1>{course?.courseName}</h1>
+                <p>{course?.category?.name}</p>
+
+                <div>
+                  <span>4.7</span>
+                  <ReactStars
+                    count={5}
+                    size={20}
+                    edit={false}
+                    activeColor="#ffd700"
+                    emtpyIcon={<GiNinjaStar />}
+                    fullIcon={<GiNinjaStar />}
+                  />
+                  <span>{course?.ratingAndReviews?.length} Ratings</span>
+                </div>
+
+              </div>
+            </div>
+            <div>
+              <button>
+               <RiDeleteBin6Line/>
+               <span>remove</span>
+              </button>
+              <p>Rs {course?.price}</p>
+            </div>
+            
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default RenderCartCourses;
--- client/src/Component/Core/Dashboard/Cart/RenderTotalAmount.jsx
+++ client/src/Component/Core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
+import IconBtn from "../../../Common/IconBtn"
 const RenderTotalAmount = () => {
+  const {total, cart} = useSelector((state)=>state.cart);
+  const handleBuyCourse=()=>{
+     
+  }
   return (
     <>
-    <div>RenderTotalAmount</div>
+    <div>
+      <h1>Total</h1>
+      <p>Rs {total}</p>
+
+      <IconBtn text="Buy now" onclick={handleBuyCourse} customClasses={"w-full justify-center"}/>
+    </div>
     </>
   )
 }
